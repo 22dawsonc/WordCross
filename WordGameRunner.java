d2c84ad7a0e0c87cf4222c94be21b6ee230cd7b4
Dawson Chen <22dawsonc@students.harker.org>
2019-03-31 09:32:29 -0700

    finish app

---
+++ WordGameRunner.java
@@ -0,0 +1,7 @@
+public class WordGameRunner {
+    public static void main(String[] args) {
+        System.out.println("running");
+        WordGame game = new WordGame();
+        game.play();
+    }
+}
